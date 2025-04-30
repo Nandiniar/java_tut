e1e37e308b9c462fcc572cc63f1557044b1bc708
nandini <nandani@contaque.com>
2025-04-30 12:13:13 +0530

    Please add this
    Signed-off-by: nandini <nandani@contaque.com>

---
+++ course/1hello.java
@@ -0,0 +1,16 @@
+
+// to run the file , first we have to compile it
+// java is platform independent because of jvm(java virtual machine)
+// jvm exceute the java code but jvm will not accept the code directly
+// java is platform independent means ke java program can be run in any machine but our system should have jvm
+// jvm me bhejne se phale hume byte code me convert karna padhta hai because jvm only understand the byte code
+// in all the file the ecxution will start from main
+
+// compiler java ke code ko byre code me convert karwata hai and then jvm looks for main part then only it will accept
+// when we compile the code then another class will be created 
+class Hello{
+public static void main(String a[])
+{
+    System.out.println("Hello world");
+}
+}
